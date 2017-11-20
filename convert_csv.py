b50b9eaa9f05de4be8d2d439e2e0332fd16044ba
kawarayu <kawarayu@gmail.com>
2017-11-20 09:37:10 +0900

    Create convert_csv.py

---
+++ convert_csv.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+day = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+input_file = 'user.csv'
+output_file = day + '_user.csv'
+c1 = ['ログイン名', '名前']
+c2 = ['login_name', 'name']
+df = pd.read_csv(input_file, encoding='utf-8')
+df2 = df.loc[:, c1]
+df2.columns = c2
+df2.to_csv(output_file, encoding='utf-8', index=False)
