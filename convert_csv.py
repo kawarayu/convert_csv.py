ceb922f1e3ced3ea068baa1590b01d94dfc35929
kawarayu <kawarayu@gmail.com>
2017-11-22 22:22:11 +0900

    UnicodeDecodeErrorへの対応

--- convert_csv.py
+++ convert_csv.py
@@ -6,7 +6,10 @@ input_file = 'user.csv'
 output_file = day + '_user.csv'
 c1 = ['ログイン名', '名前']
 c2 = ['login_name', 'name']
-df = pd.read_csv(input_file, encoding='utf-8')
+#df = pd.read_csv(input_file, encoding='utf-8')
+with codecs.open(input_file, 'r', 'shift-jis', 'ignore') as f:
+  df = pd.read_table(f, delimiter=',')
+# ENCODING = 'cp932'
 df2 = df.loc[:, c1]
 df2.columns = c2
 df2.to_csv(output_file, encoding='utf-8', index=False)
